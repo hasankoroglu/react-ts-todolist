16851e6b21b8a1a85e80ab992f43d6cd8a2f72d3
Hasan KÖROĞLU <hasan@hasankoroglu.com>
2020-01-26 01:11:36 +0300

    Updates

---
+++ precache-manifest.dad5ad7b5de8dd4ae465add991d946d7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d089eddd970c8c365371fde915de9cd",
+    "url": "/react-ts-todolist/index.html"
+  },
+  {
+    "revision": "656e8f96209a9542d19e",
+    "url": "/react-ts-todolist/static/css/main.35aa8cc2.chunk.css"
+  },
+  {
+    "revision": "91ac6b8937bd225694f5",
+    "url": "/react-ts-todolist/static/js/2.b435b02c.chunk.js"
+  },
+  {
+    "revision": "656e8f96209a9542d19e",
+    "url": "/react-ts-todolist/static/js/main.8dd2743c.chunk.js"
+  },
+  {
+    "revision": "28dde07389a531881e68",
+    "url": "/react-ts-todolist/static/js/runtime-main.82f19769.js"
+  }
+]);
